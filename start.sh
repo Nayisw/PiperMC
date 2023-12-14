de2dcdcb5dcac27ed77b9a6f90f93e010bcce09f
Nayisw <mrprogaming003@gmail,com>
2023-12-14 22:10:38 +0530

    commit

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-chmod +x script
+
 
 ### THIS PROJECT IS OPEN SOURCE
 ### FEEL FREE TO CONTRIBUTE TOWARD THIS PROJECT
 ### PLEASE CREDIT THE OWNER OF THE PROJECT :)
 ### Script made by @Nayisw
-
-
 
 ### <----- RAM CONFIGURATION
 
@@ -14,27 +12,17 @@ ram='1G' #   <---- CHANGE THIS VALUE TO YOUR DESIRED VALUE
 
 ### RAM CONFIGURATION ----->
 
-
-
 ###  [ !!! DO NOT CHANGE ANYTHING BELOW THIS !!! ]
 
-
-
-
-
-
-
-
-
-
 # Check and install dependencies
+sudo chmod +x *
 dependencies() {
     if ! command -v java &>/dev/null; then
-        sudo apt install opendjdk-17-jre-headless -y
+        sudo apt install default-jdk -y
     fi
 
-    if ! command -v Screen &>/dev/null; then
-        sudo apt install Screen -y
+    if ! command -v screen &>/dev/null; then
+        sudo apt install screen -y
     fi
 
     if ! command -v jq &>/dev/null; then
@@ -58,20 +46,61 @@ dependencies() {
 dependencies
 echo -e "\e[32mDependencies Installed!\e[0m"
 
+# Funtion to install the server
+install_paper() {
+
+    echo -e "\e[32mVisit https://papermc.io/api/v2/projects/paper to see supported versions\e[0m"
+
+    echo -e "\e[33mLeave blank and press Enter to select the latest version by default\e[0m"
+    echo -e "\e[34mEnter your version: \e[0m"
+    read MC_VERSION
+
+    API_URL="https://papermc.io/api/v2/projects/paper"
+
+    if [ -z "$MC_VERSION" ]; then
+        LATEST_VERSION=$(curl -s "${API_URL}" | jq -r '.versions' | jq -r '.[-1]')
+        MC_VERSION="${LATEST_VERSION}"
+        LATEST_BUILD=$(curl -s "${API_URL}/versions/${LATEST_VERSION}" | jq -r '.builds | .[-1]')
+        VERSION="${LATEST_VERSION}-${LATEST_BUILD}"
+        DOWNLOAD_URL="${API_URL}/versions/${LATEST_VERSION}/builds/${LATEST_BUILD}/downloads/paper-${VERSION}.jar"
+    else
+        VER_BUILD=$(curl -s "${API_URL}/versions/${MC_VERSION}" | jq -r '.builds | .[-1]')
+        VERSION="${MC_VERSION}-${VER_BUILD}"
+        DOWNLOAD_URL="${API_URL}/versions/${MC_VERSION}/builds/${VER_BUILD}/downloads/paper-${VERSION}.jar"
+    fi
+
+    mkdir -p "$PWD/server" && cd server
+    wget -O eula.txt https://cdn.discordapp.com/attachments/772003388455125002/1148153568877097062/eula.txt #EULA
+
+    curl -o start.jar -L "${DOWNLOAD_URL}"
+
+    echo -e "\e[33mPaperMC $VERSION downloaded successfully!\e[0m"
+}
+echo -e "\e[32mSuccessfully installed server!\e[0m"
+
 # Check if server is installed
+if [ ! -d server ]; then
+    echo -e "\e[33m Server not installed!\nInstall a server to continue...\e[0m"
 
-if [ ! -d $PWD/server ]; then
-    echo -e "\e[33m Server not installed!\nInstall a server to continue\e[0m"
+    echo -e "\95m Do you want to install the server right now?\e[0m"
+    echo "1. Yes"
+    echo "2. No"
+    read yn
+    if $yn -eq 1; then
+        install_paper
+    elif $yn -eq 2; then
+        exit 0
+    fi
 
-    . script/install.sh
-    install_paper
 fi
 
 # Start the server
 start_server() {
-    jar="$PWD/server/start.jar"
+    cd server 
+    jar="start.jar"
+    BPYTOP=$HOME/.local/bin/bpytop
 
-    screen -S afk -d -m bpytop
+    screen -S afk -d -m $BPYTOP
 
     screen -S server -d -m java -Xms${ram} -Xmx${ram} -jar ${jar} nogui
     screen -r server
@@ -80,9 +109,11 @@ start_server() {
 
 # Menu
 main() {
-    echo "=== Main Menu ==="
+    echo "==============================="
+    echo "Server Menu"
+    echo "-------------------------------"
     echo "1. Start Server"
-    echo "2. Other Options" # You can add more options here
+    echo "2. Install Playit.gg"
     echo "3. Exit"
 
     read -rp "Enter your choice: " choice
@@ -93,18 +124,24 @@ main() {
         start_server
         ;;
     2)
-        echo "Performing other options..."
+        echo "Closing the server..."
+        killall screen
+        clear
+        echo -e "\e32mType './start.sh' to start the server again!"
+        ;;
+    3)
+        echo "Installing Playit.gg"
 
-        echo "1.Java config"
-        read config
-
-        if $config -eq 1; then
-            . script/java.sh
-            java
+        if [ -d server/plugins ]; then
+            cd server/plugins
+            wget https://github.com/playit-cloud/playit-minecraft-plugin/releases/latest/download/playit-minecraft-plugin.jar
+        else
+            echo "Please start the server once to install Playit.gg"
         fi
 
         ;;
-    3)
+
+    4)
         echo "Exiting..."
         exit 0
         ;;
@@ -114,3 +151,4 @@ main() {
     esac
 
 }
+main
